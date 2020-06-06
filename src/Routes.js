49aaf346507f0675ebb3a56daf5b10c46557baca
Sneha-jay <snehajpandiyan10@gmail.com>
2020-06-06 18:28:34 +0530

    Building our React App

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,32 @@
 import React from "react";
+import Signup from "./containers/Signup";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import Settings from "./containers/Settings";
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      {/* Finally, catch all unmatched routes */}
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
+    <UnauthenticatedRoute exact path="/login">
+  <Login />
+</UnauthenticatedRoute>
+<UnauthenticatedRoute exact path="/signup">
+  <Signup />
+</UnauthenticatedRoute>
+<AuthenticatedRoute exact path="/settings">
+  <Settings />
+</AuthenticatedRoute>
+<AuthenticatedRoute exact path="/notes/new">
+  <NewNote />
+</AuthenticatedRoute>
+<AuthenticatedRoute exact path="/notes/:id">
+  <Notes />
+</AuthenticatedRoute>
+</Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  STRIPE_KEY: "pk_test_gZcKOH5tyH42c8Kru0RUjGa600NUcLpQRH",
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-uploads-cloud-project"
